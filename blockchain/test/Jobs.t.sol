e89b76dfb51dfb3bb5ffa794d14ed04bf65cdf56
Sergi Roca Laguna <sergiroca@163.com>
2024-03-16 16:57:48 +0100

    update solidity version

--- blockchain/test/Jobs.t.sol
+++ blockchain/test/Jobs.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Jobs} from "../src/Jobs.sol";
@@ -48,7 +48,7 @@ contract JobsTest is Test {
             clientId,
             isUnassigned
         );
-        
+
         (
             ,
             string memory jobOriginalId,
